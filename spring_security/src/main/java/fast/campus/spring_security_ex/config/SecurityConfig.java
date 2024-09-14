cf87aab23dac7c5489cd51e338c81c34451c5c5c
hyeonjoonpark <pjjoon1379@gmail.com>
2024-09-14 23:43:54 +0900

    CustomAuthManager

--- spring_security/src/main/java/fast/campus/spring_security_ex/config/SecurityConfig.java
+++ spring_security/src/main/java/fast/campus/spring_security_ex/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package fast.campus.spring_security_ex.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,6 +15,10 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+
+import java.util.Collection;
+import java.util.function.Supplier;
 
 @Configurable
 public class SecurityConfig {
@@ -33,13 +41,26 @@ public class SecurityConfig {
         return NoOpPasswordEncoder.getInstance(); // NoOPPasswordEncoder는 비밀번호 평문을 그대로 비교 -> deprecated
     }
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(
-                        r -> r.anyRequest().hasAnyAuthority("READ", "WRITE") // READ, WRITE 권한이 있는 사용자만 허용
+                        // r -> r.anyRequest().hasAnyAuthority("READ", "WRITE") // READ, WRITE 권한이 있는 사용자만 허용
+                        r -> r.anyRequest().access(customAuthManager())
                 );
-
         return http.build();
+    }
+
+    private AuthorizationManager<RequestAuthorizationContext> customAuthManager() {
+        return new AuthorizationManager<RequestAuthorizationContext>() {
+            @Override
+            public AuthorizationDecision check(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
+                Authentication auth = authentication.get();
+                Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+                boolean granted = authorities.stream().anyMatch(each -> each.getAuthority().equals("READ"));
+                return new AuthorizationDecision(granted);
+            }
+        };
     }
 }
