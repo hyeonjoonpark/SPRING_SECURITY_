ad422720f517950f17fb31d31d9a7dd2f0768db5
hyeonjoonpark <pjjoon1379@gmail.com>
2024-09-14 21:32:14 +0900

    FEAT(config) : SecurityConfig 엔드포인트 설정

--- spring_security/src/main/java/fast/campus/spring_security_ex/config/SecurityConfig.java
+++ spring_security/src/main/java/fast/campus/spring_security_ex/config/SecurityConfig.java
@@ -2,12 +2,15 @@ package fast.campus.spring_security_ex.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configurable
 public class SecurityConfig {
@@ -28,5 +31,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance(); // NoOPPasswordEncoder는 비밀번호 평문을 그대로 비교 -> deprecated
+    }
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                        r -> r.anyRequest().hasAnyAuthority("READ", "WRITE") // READ, WRITE 권한이 있는 사용자만 허용
+                );
+
+        return http.build();
     }
 }
