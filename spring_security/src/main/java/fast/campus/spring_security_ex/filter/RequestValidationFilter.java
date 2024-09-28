7f63944f6a70f3f2ea8575a9bbb0c5611ff25c86
hyeonjoonpark <pjjoon1379@gmail.com>
2024-09-29 00:13:56 +0900

    FEAT(filter): RequestValidationFilter
    request 검증 필터를 작성하였습니다

---
+++ spring_security/src/main/java/fast/campus/spring_security_ex/filter/RequestValidationFilter.java
@@ -0,0 +1,37 @@
+package fast.campus.spring_security_ex.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class RequestValidationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String requestId = request.getHeader("Request-Id");
+
+        if(requestId == null || requestId.isBlank()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
